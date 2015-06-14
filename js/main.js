51716bc0e32b32ebf2707c9dfe040091c36fec19
Muriel Silveira <murielkong@gmail.com>
2015-06-14 18:36:37 -0300

    Utilizado o Materialize na interface, e melhorada a interação com as questões.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-'use strict';
+var Quiz = (function() {
+    'use strict';
 
-var questoes = [
+    var questoes = [
         {
-            titulo: 'Primeira questão?',
+            titulo: 'Qual o elemento químico mais abundante do universo?',
             alternativas: [
                 'Alternativa 1.1',
                 'Alternativa 1.2',
@@ -22,55 +23,81 @@ var questoes = [
             ]
         }
     ],
-    questao_atual;
+    questao_atual,
+    $btnVoltar = $('#btn-voltar'),
+    $btnProxima = $('#btn-proxima');
 
+    function start() {
+        $btnVoltar.removeClass('hide').click(mostrarQuestaoAnterior);
+        $btnProxima.removeClass('hide').click(mostrarProximaQuestao);
 
-function init() {
-    mostrarPrimeiraQuestao();
+        mostrarPrimeiraQuestao();
+    }
 
-    $('#btn-voltar').click(mostrarQuestaoAnterior);
-    $('#btn-proxima').click(mostrarProximaQuestao);
-}
-
-function mostrarPrimeiraQuestao() {
-    questao_atual = 0;
-    mostrarQuestao(questoes[questao_atual]);
-}
-
-function mostrarProximaQuestao() {
-    if (questao_atual + 1 < questoes.length) {
-        questao_atual++;
+    function mostrarPrimeiraQuestao() {
+        questao_atual = 0;
         mostrarQuestao(questoes[questao_atual]);
-    } else {
-        console.log('não há mais questões');
-    }
-}
-
-function mostrarQuestaoAnterior() {
-    if (questao_atual > 0) {
-        questao_atual--;
-        mostrarQuestao(questoes[questao_atual]);
-    } else {
-        console.log('não há mais questões');
-    }
-}
-
-function mostrarQuestao(questao) {
-    var tituloHtml = '',
-        alternativasHtml = '',
-        numeroDeAlternativas = questao.alternativas.length;
-
-    tituloHtml = '<h2>' + questao.titulo + '</h2>';
-
-    for (var i = 0; i < numeroDeAlternativas; i++) {
-        alternativasHtml += '<div>' +
-            '<input type="radio" name="alternativa" id="alternativa_' + i + '">' +
-            '<span class="texto-alternativa">' + questao.alternativas[i] + '</span>' +
-            '</div>';
     }
 
-    $('#questao-title').html(tituloHtml);
-    $('#questao-content').html(alternativasHtml);
-}
+    function mostrarProximaQuestao() {
+        if (questao_atual + 1 < questoes.length) {
+            questao_atual++;
+            mostrarQuestao(questoes[questao_atual]);
+        }
+    }
 
-init();
+    function mostrarQuestaoAnterior() {
+        if (questao_atual > 0) {
+            questao_atual--;
+            mostrarQuestao(questoes[questao_atual]);
+        }
+    }
+
+    function mostrarQuestao(questao) {
+        var tituloHtml = '',
+            alternativasHtml = '',
+            numeroDeAlternativas = questao.alternativas.length;
+
+        tituloHtml = '<h4>' + (questao_atual + 1) + '. ' + questao.titulo + '</h4>';
+
+        for (var i = 0; i < numeroDeAlternativas; i++) {
+            alternativasHtml += '<div class="alternativa">' +
+                '<input type="radio" name="alternativa" id="alternativa_' + i + '">' +
+                '<label for="alternativa_' + i + '">' + questao.alternativas[i] + '</label>' +
+                '</div>';
+        }
+
+        $('#questao-title').html(tituloHtml);
+        $('#questao-content').html(alternativasHtml);
+        $('.alternativa input').click(selecionarAlternativa);
+        if (questao.resposta !== undefined) {
+            $('#alternativa_' + questao.resposta).prop('checked', true);
+        }
+
+        ajustarBotoes();
+    }
+
+    function ajustarBotoes() {
+        $btnVoltar.prop('disabled', false);
+        $btnProxima.prop('disabled', false);
+
+        if (questao_atual === 0) {
+            $btnVoltar.prop('disabled', true);
+        }
+
+        if (questao_atual + 1 === questoes.length) {
+            $btnProxima.prop('disabled', true);
+        }
+    }
+
+    function selecionarAlternativa() {
+        var resposta = parseInt($(this).attr('id').split('_')[1]);
+        questoes[questao_atual].resposta = resposta;
+    }
+
+    return {
+        start: start
+    };
+})();
+
+Quiz.start();
